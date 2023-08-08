dc2bf4f33f0d829b2ca61041d23853c3dd453250
NathanBaldwin <nathanrobertbaldwin@gmail.com>
2023-08-08 13:29:33 -0500

    Simplifying database: Removed equipment slots from character.

--- app/models/inventory.py
+++ app/models/inventory.py
@@ -1,26 +1,23 @@
 from .db import db, environment, SCHEMA, add_prefix_for_prod
 
+
 class Inventory(db.Model):
-    __tablename__ = 'inventories'
+    __tablename__ = "inventories"
 
     if environment == "production":
-        __table_args__ = {'schema': SCHEMA}
+        __table_args__ = {"schema": SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    character_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("characters.id")))
+    character_id = db.Column(
+        db.Integer, db.ForeignKey(add_prefix_for_prod("characters.id"))
+    )
     equipment_id = db.Column(
         db.Integer, db.ForeignKey(add_prefix_for_prod("equipments.id"))
     )
 
     def to_dict(self):
         return {
-            'id': self.id,
-            'character_id': self.character_id,
-            'equipment_id': self.equipment_id
+            "id": self.id,
+            "character_id": self.character_id,
+            "equipment_id": self.equipment_id,
         }
-        
-        
-
-
-
-
