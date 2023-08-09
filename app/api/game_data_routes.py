8d348c20d0eeb77c188b3ae22858474e031ce130
NathanBaldwin <nathanrobertbaldwin@gmail.com>
2023-08-09 10:12:38 -0500

    Fixed bug with game data shape.

--- app/api/game_data_routes.py
+++ app/api/game_data_routes.py
@@ -1,21 +1,18 @@
 from flask import Blueprint, jsonify
-from flask_login import login_required
 from app.models import Equipment, Monster, Attack
-from flask_login import current_user
 from .utilities import normalizer
 
 game_data_routes = Blueprint("game_data_routes", __name__)
 
 
 @game_data_routes.route("/")
-# @login_required
 def game_data():
     """
     Query for all game data. Returns game data somehow. UPDATE THIS.
     """
     equipment_data = Equipment.query.all()
     monster_data = Monster.query.all()
-    monster_attacks_data = Monster.query.all()
+    monster_attacks_data = Attack.query.filter(Attack.owner_type == "monster")
 
     equipment_array = [item.to_dict() for item in equipment_data]
     monsters_array = [monster.to_dict() for monster in monster_data]
