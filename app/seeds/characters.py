2c0f04f9086f302e779a1df8ec999c5c4ca4a1b5
NathanBaldwin <nathanrobertbaldwin@gmail.com>
2023-08-08 19:01:45 -0500

    css mostly work.

---
+++ app/seeds/characters.py
@@ -0,0 +1,32 @@
+from app.models import db, Character, environment, SCHEMA
+from sqlalchemy.sql import text
+from .data.characters_data import characters
+
+
+# Adds a demo user, you can add other users here if you want
+def seed_characters():
+    for character in characters:
+        new_character = character(
+            user_id=character["user_id"],
+            name=character["name"],
+            algorithms=character["algorithms"],
+            databases=character["databases"],
+            css=character["css"],
+            debugging=character["debugging"],
+            energy=character["energy"],
+        )
+
+        db.session.add(new_character)
+
+    db.session.commit()
+
+
+def undo_characters():
+    if environment == "production":
+        db.session.execute(
+            f"TRUNCATE table {SCHEMA}.characters RESTART IDENTITY CASCADE;"
+        )
+    else:
+        db.session.execute(text("DELETE FROM characters"))
+
+    db.session.commit()
