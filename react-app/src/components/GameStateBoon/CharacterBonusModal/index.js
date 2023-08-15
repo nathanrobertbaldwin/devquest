a96b056e540e450433d57209180604ebe3cf2527
NathanBaldwin <nathanrobertbaldwin@gmail.com>
2023-08-14 20:49:37 -0500

    gameStateBoon - CharacterBonusModal ready to be hooked up to backend.

--- react-app/src/components/GameStateBoon/CharacterBonusModal/index.js
+++ react-app/src/components/GameStateBoon/CharacterBonusModal/index.js
@@ -19,173 +19,177 @@ export default function CharacterBonusModal({ stats }) {
   const baseDebugging = char.debugging;
   const baseMaxEnergy = char.maxEnergy;
 
+  // tomorrow test update of character.
+
   return (
     <>
       <div id="character-attributes-container">
-        <div id="cc-attributes-container">
-          <div className="attribute-button-container">
-            <span>Backend: {backend}</span>
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={() => {
-                  if (points > 0) {
-                    setBackend(backend + 1);
-                    setPoints(points - 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  if (backend > 0) {
-                    setBackend(backend - 1);
-                    setPoints(points + 1);
-                  }
-                }}
-              >
-                -
-              </button>
+        <form>
+          <div id="cc-attributes-container">
+            <div className="attribute-button-container">
+              <span>Backend: {backend}</span>
+              <div className="button-container">
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (points > 0) {
+                      setBackend(backend + 1);
+                      setPoints(points - 1);
+                    }
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (backend > baseBackend) {
+                      setBackend(backend - 1);
+                      setPoints(points + 1);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="attribute-button-container">
+              <span>Frontend: {frontend}</span>
+              <div className="button-container">
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (points > 0) {
+                      setFrontend(frontend + 1);
+                      setPoints(points - 1);
+                    }
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (frontend > baseFrontend) {
+                      setFrontend(frontend - 1);
+                      setPoints(points + 1);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="attribute-button-container">
+              <span>Algorithms: {algorithms}</span>
+              <div className="button-container">
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (points > 0) {
+                      setAlgorithms(algorithms + 1);
+                      setPoints(points - 1);
+                    }
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (algorithms && baseAlgorithms) {
+                      setAlgorithms(algorithms - 1);
+                      setPoints(points + 1);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="attribute-button-container">
+              <span>CSS: {CSS}</span>
+              <div className="button-container">
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (points > 0) {
+                      setCSS(CSS + 1);
+                      setPoints(points - 1);
+                    }
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (CSS > baseCSS) {
+                      setCSS(CSS - 1);
+                      setPoints(points + 1);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="attribute-button-container">
+              <span>Debugging: {debugging}</span>
+              <div className="button-container">
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (points > 0) {
+                      setDebugging(debugging + 1);
+                      setPoints(points - 1);
+                    }
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (debugging > baseDebugging) {
+                      setDebugging(debugging - 1);
+                      setPoints(points + 1);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="attribute-button-container">
+              <span>Max Energy: {maxEnergy}</span>
+              <div className="button-container">
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (points > 0) {
+                      setMaxEnergy(maxEnergy + 10);
+                      setPoints(points - 1);
+                    }
+                  }}
+                >
+                  +
+                </button>
+                <button
+                  type="button"
+                  onClick={() => {
+                    if (maxEnergy > baseMaxEnergy) {
+                      setMaxEnergy(maxEnergy - 10);
+                      setPoints(points + 1);
+                    }
+                  }}
+                >
+                  -
+                </button>
+              </div>
             </div>
           </div>
-          <div className="attribute-button-container">
-            <span>Frontend: {frontend}</span>
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={() => {
-                  if (points > 0) {
-                    setFrontend(frontend + 1);
-                    setPoints(points - 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  if (frontend > 0) {
-                    setFrontend(frontend - 1);
-                    setPoints(points + 1);
-                  }
-                }}
-              >
-                -
-              </button>
-            </div>
-          </div>
-          <div className="attribute-button-container">
-            <span>Algorithms: {algorithms}</span>
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={() => {
-                  if (points > 0) {
-                    setAlgorithms(algorithms + 1);
-                    setPoints(points - 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  if (algorithms > 0) {
-                    setAlgorithms(algorithms - 1);
-                    setPoints(points + 1);
-                  }
-                }}
-              >
-                -
-              </button>
-            </div>
-          </div>
-          <div className="attribute-button-container">
-            <span>CSS: {CSS}</span>
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={() => {
-                  if (points > 0) {
-                    setCSS(CSS + 1);
-                    setPoints(points - 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  if (CSS > 0) {
-                    setCSS(CSS - 1);
-                    setPoints(points + 1);
-                  }
-                }}
-              >
-                -
-              </button>
-            </div>
-          </div>
-          <div className="attribute-button-container">
-            <span>Debugging: {debugging}</span>
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={() => {
-                  if (points > 0) {
-                    setDebugging(debugging + 1);
-                    setPoints(points - 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  if (debugging > 0) {
-                    setDebugging(debugging - 1);
-                    setPoints(points + 1);
-                  }
-                }}
-              >
-                -
-              </button>
-            </div>
-          </div>
-          <div className="attribute-button-container">
-            <span>Max Energy: {maxEnergy}</span>
-            <div className="button-container">
-              <button
-                type="button"
-                onClick={() => {
-                  if (points > 0) {
-                    setMaxEnergy(maxEnergy + 10);
-                    setPoints(points - 1);
-                  }
-                }}
-              >
-                +
-              </button>
-              <button
-                type="button"
-                onClick={() => {
-                  if (maxEnergy > 0) {
-                    setMaxEnergy(maxEnergy - 10);
-                    setPoints(points + 1);
-                  }
-                }}
-              >
-                -
-              </button>
-            </div>
-          </div>
-        </div>
+        </form>
       </div>
     </>
   );
